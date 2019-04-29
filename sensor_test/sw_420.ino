6538a31bd08c98f478461933d5e74736e83b476c
micaellajimini <jimin7971@gmail.com>
2019-04-29 14:37:26 +0900

    sw420 test

---
+++ sensor_test/sw_420.ino
@@ -0,0 +1,29 @@
+int ledPin =13;
+int vib =2;
+ 
+void setup(){
+  pinMode(ledPin, OUTPUT);
+  pinMode(vib, INPUT); //센서핀 입력
+  Serial.begin(9600); //시리얼통신 설정 9600
+  Serial.println("----------------------vibration demo------------------------");
+}
+void loop(){
+  long measurement =TP_init();
+  delay(50);
+  Serial.print("measurment = ");
+  Serial.println(measurement);
+  if (measurement > 1000){
+    digitalWrite(ledPin, HIGH);
+  }
+  else{
+    digitalWrite(ledPin, LOW); 
+  }
+}
+ 
+long TP_init(){
+  delay(10);
+  long measurement=pulseIn (vib, HIGH);
+  return measurement;
+}
+
+
