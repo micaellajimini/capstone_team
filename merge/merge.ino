02cf8775d24585f033bf8fc0c36125e07a01a355
jaeha <years6666@gmail.com>
2019-06-05 20:21:05 +0900

    merge code version 1

---
+++ merge/merge.ino
@@ -0,0 +1,41 @@
+const int trigPin = 2;  //D4
+const int echoPin = 0;  //D3
+int sensor = 13; // D7
+void setup() {
+  pinMode(trigPin, OUTPUT); // Sets the trigPin as an Output
+  pinMode(echoPin, INPUT); // Sets the echoPin as an Input
+  pinMode(sensor, INPUT);
+  Serial.begin(9600); // Starts the serial communication
+}
+void loop() {
+  long duration, distance = -1;
+  // Clears the trigPin
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  // Sets the trigPin on HIGH state for 10 micro seconds
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+  duration = pulseIn(echoPin, HIGH);
+  //음파속도 = 340m/s, 29us에 1cm, 왕복(2)
+  distance = duration / 29 / 2;
+    
+  // Prints the distance on the Serial Monitor
+  
+  delay(2000);
+  ///////////////////////////////////////
+  /*PIR SENSOR*/
+  long state = digitalRead(sensor);
+  int Limit=10000;
+  if (Limit < distance || state != HIGH ){//no man
+    Serial.print(distance);
+    Serial.println("cm");
+    Serial.print(state);
+    Serial.println("----------------------");
+    delay(2000);
+  }
+  else{
+      Serial.println("Motion Detected");
+      delay(2000);
+  }  
+}
--- sensor_vi/sensor_vi2.ino/sensor_vi2.ino.ino
+++ sensor_vi/sensor_vi2.ino/sensor_vi2.ino.ino
@@ -1,17 +1,58 @@
-const int analogInPin = A0;
-char buffer[50];
-int soundSensor = 0;
-float voltSensor = 0.0;
-void setup()
-{
-Serial.begin(115200);
-}
-void loop()
-{
-  soundSensor = analogRead(analogInPin);
-  Serial.print("Analog = " );
-  Serial.print(soundSensor);
-  Serial.print("\n");
-  delay(500);
+/* Processing code */
 
+import processing.serial.*;
+
+Serial myPort;        // The serial port
+
+int[] val = new int[64];
+
+void setup () {
+  // set the window size:
+  size(1024, 1024);
+
+  String portName = Serial.list()[2];  // Set your port correctly
+  myPort = new Serial(this, portName, 115200);
+  
+  // don't generate a serialEvent() unless you get a newline character:
+  myPort.bufferUntil('\n');
+
+  // set inital background:
+  background(255);
+  rect(0, 0, 1024, 1024);
+
+  // set stroke line color to red.
+  stroke(255,0,0);
+  
+  //delay for arduino serial reboot.
+  delay(2000);
+}
+
+void draw () {
+    //Clear background for re-drawing.
+    background(255);
+
+    //Draw stroke for each frequency 
+    for (int i =0; i<64; i++){
+      line(i*16 +1,height,i*16 +1,height-val[i]*4);
+    }
+}
+
+
+void serialEvent (Serial myPort) {
+  try{
+    
+      //Create buffer for data saving
+      byte[] inByte = new byte[200];
+      
+      //Put data into buffer
+      myPort.readBytesUntil('\n',inByte);
+      
+      // convert to an int and map to the screen height:
+      for(int i = 0; i<64; i++){
+       val[i] = int(inByte[i]);      
+      }
+
+  }catch(Exception e){
+    println("Err");
+  }
 }
