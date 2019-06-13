689abe0583436c8068d14bac6b43fbe681452b53
micaellajimini <jimin7971@gmail.com>
2019-06-13 05:15:31 +0000

    add line api

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const request = require('request')
+const config = require('./config_cap');
+
+const TARGET_URL = 'https://notify-api.line.me/api/notify'
+
+var app = express();
+
+//router name set needed
+app.get('/',function(req,res){
+	request.post({
+		url: TARGET_URL,
+		headers: {
+			'Authorization': `Bearer ${config.TOKEN}`
+		},
+		form: {
+			//change message
+			message: 'messaging_test - jimin'
+		}
+	}, (error, response, body) => {
+		console.log(body)
+	})	
+});
+
+app.listen(8000, function(){
+//	console.log(config.TOKEN);
+	console.log('listening on port 8000!');		
+});
+
+
